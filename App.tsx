22afeeaa280490b9bc52ed288da5c71eaf764c42
jpedroc <jpedrocalixto@outlook.com>
2024-11-25 08:21:00 -0300

    NT8 - first run device

--- App.tsx
+++ App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import NutriTrainScreen from './src/screens/Auth/NutriTrainScreen';
 
-// Define os tipos das rotas
-type RootStackParamList = {
-    Login: undefined;
-    Home: undefined;
-    Register: undefined;
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="NutriTrain"
+          component={NutriTrainScreen}
+          options={{ headerShown: false }} // Esconde o cabeçalho
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-                {/* Adicione as outras telas conforme necessário */}
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
+export default App;
